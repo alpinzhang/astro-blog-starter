7ad3778c5e01d2d6dff5370eb41f17f6734f8440
zhangxingping <ping.zx@qq.com>
2024-02-06 14:35:08 +0800

    fix: 重构

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config';
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://example.com",
-  integrations: [sentry(), spotlightjs()]
-});
+export default defineConfig({});
--- src/scripts/menu.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".nav-links").classList.toggle("expanded");
-});
-console.log(1111);
