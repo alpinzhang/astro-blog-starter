15947880d15c08e59b1a26179c79eaadf716dfba
zhangxingping <ping.zx@qq.com>
2024-02-06 14:17:51 +0800

    fix: remove vue ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
-import vue from "@astrojs/vue";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [sentry(), spotlightjs(), vue()]
+  integrations: [sentry(), spotlightjs()]
 });
